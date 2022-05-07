6706cae35c8b08b1b622d8d5b210040720f7f94f
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2022-05-07 15:25:22 +0200

    events

--- store/actions/EventActions.ts
+++ store/actions/EventActions.ts
@@ -5,7 +5,7 @@ export const fetchEvents = () => {
         const idToken = getState().user.idToken
 
         const response = await fetch(
-            'https://react-exam-68375-default-rtdb.europe-west1.firebasedatabase.app/chatrooms.json?auth='
+            'https://react-exam-68375-default-rtdb.europe-west1.firebasedatabase.app/events.json?auth='
             + idToken, {
 
             method: 'GET',
@@ -21,13 +21,13 @@ export const fetchEvents = () => {
             //There was a problem..
         } else {
 
-            let chatrooms = [];
-            for (const key in data) {
-                let chatroom = new Chatroom(data[key].chatroomName, data[key].chatmessages, '', key)
-                chatrooms.push(chatroom)
-            }
+            // let chatrooms = [];
+            // for (const key in data) {
+            //     let chatroom = new Chatroom(data[key].chatroomName, data[key].chatmessages, '', key)
+            //     chatrooms.push(chatroom)
+            // }
 
-            dispatch({ type: FETCH_EVENTS, payload: chatrooms })
+            dispatch({ type: FETCH_EVENTS, payload: 'hah' })
         }
     };
 }
--- store/reducers/EventReducer.js
+++ store/reducers/EventReducer.js
@@ -6,39 +6,14 @@ const initialState = {
 
 
 const eventReducer = (state = initialState, action) => {
-    // switch (action.type) {
+    switch (action.type) {
         
-    //     case FETCH_CHATROOMS:
-    //         return { ...state, chatrooms: action.payload }
+        case FETCH_EVENTS:
+            return { ...state, events: action.payload }
 
-    //     case ADD_CHATROOM:
-    //         console.log(action.payload); // Should print out the chatroomName
-    //         //state.chatrooms.push(chatroom); // mutate chatrroms array! Not Allowed!
-
-    //         const chatroom = new Chatroom(action.payload.chatroomName, [], './../assets/chat-img/chat-img-sm.png', action.payload.id);
-    //         // const chatroom = { title: action.payload, chatmessages: [], imageUrl: ''}
-
-    //         const newChatroomArray = [...state.chatrooms, chatroom];
-    //         return { ...state, chatrooms: newChatroomArray };
-
-    //     case DELETE_CHATROOM:
-    //         console.log(action.payload);
-    //         return {
-    //             ...state, chatrooms:
-    //                 state.chatrooms.filter(chatroom => chatroom.id !== action.payload)
-    //         };
-
-    //     case SEND_MESSAGE:
-    //         console.log(action.payload.oneMessage.text)
-    //         // chatroom = state.chatrooms.filter(chatroom => chatroom.id == action.payload.chatId)
-    //         // console.log(chatroom.title)
-    //         // return { ...state, chatroom.chatmessages:  }
-
-    //     //return {...state, chatrooms: [...state.chatrooms, {title: action.payload}]}
-
-    //     default:
-    //         return state; //does not do anything yet​   
-    // }
+        default:
+            return state; //does not do anything yet​   
+    }
 };
 
 export default eventReducer;

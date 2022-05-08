55d417d5fbea00f7ca34b8ef306151f9186f6c96
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2022-05-08 11:57:35 +0200

    Feed

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -9,24 +9,39 @@ const HomeScreen = ({ navigation }: { navigation: any }) =>  {
 
     const dispatch = useDispatch();
     const events = useSelector((state: RootState) => state.event.events);
-    console.log(events[0].title)
 
     useEffect(() => {
         dispatch(fetchEvents())
     }, []);
 
-    const renderItem = ({ item }: { item: any }) => (
-        
+    const imageMap = {
+        'cbs-yoga.png' : require('./../assets/cbs-yoga.png'),
+        'cbs-ghost-world.png' : require('./../assets/cbs-ghost-world.png'),
 
+      }
+
+    const renderItem = ({ item }: { item: any }) => (
+    
         <TouchableOpacity 
             onPress={() => navigation.navigate('OneChatroom', {
                 chatId: item.id,
                 chatName: item.title,
             })}>
 
-            <View>
-                <ImageBackground source={require('./../assets/cbs-yoga.png')} style={styles.feed_img}>
-                    <Text>Inside</Text>
+            <View style={styles.feed_wrapper}>
+                <ImageBackground source={imageMap[item.img as keyof typeof imageMap]} style={styles.feed_img}>
+                    <Text style={[styles.white_text, styles.title]}>{item.title}</Text>
+                    <Text style={[styles.white_text, styles.organizer]}>{item.organizer}</Text>
+                    <Text style={styles.white_text}>
+                        <Image    
+                            source={require('./../assets/icons8-alarm_clock/icons8-alarm_clock.png')}
+                        /> {item.time}
+                    </Text>
+                    <Text style={styles.white_text}>
+                        <Image    
+                            source={require('./../assets/icons8-marker/icons8-marker.png')}
+                        /> {item.place}
+                    </Text>
                 </ImageBackground>
             </View>
             
@@ -41,9 +56,27 @@ const HomeScreen = ({ navigation }: { navigation: any }) =>  {
 }
 
 const styles = StyleSheet.create({
+    feed_wrapper: {
+        flex: 1,
+        marginTop: 24,
+        alignItems: 'center',
+    },
     feed_img: {
         width: 337,
         height: 190,
+        justifyContent: 'flex-end',
+        padding: 10,
+    },
+    white_text: {
+        color: '#ffffff',
+    },
+    organizer: {
+        fontSize: 12,
+        paddingVertical: 5,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
     }
 });
 
